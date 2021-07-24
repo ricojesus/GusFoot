37cf0279662b50c4c4c2b20e0ed09af7ea6d2521
Ricardo Jesus <ricojesus@hotmail.com>
2021-07-24 17:31:51 -0300

    pacote em desenvolvimento

--- models/partida.php
+++ models/partida.php
@@ -3,11 +3,16 @@ include_once("dal/Sql.php");
 include_once("time.php");
 
 class Partida{
-    public function jogar($id_servidor, $id_liga, $temporada, $rodada, $id_time1, $id_time2){
-        $time1 = Time::getbyId($id_time1);
-        $time2 = Time::getbyId($id_time2);
+    public function jogar($id_servidor, $id_liga, $temporada, $rodada, $id_time1, $id_time2){        
+        $time1 = Time::getbyId($id_servidor, $id_time1);
+        $time2 = Time::getbyId($id_servidor, $id_time2);
+        $tempo = 0;
 
-        echo "rodou";
+        var_dump($time1);
+        //var_dump($time2);
+
+        // calcular forca do time com base em cada uma das posições gol, defessa e ataque
+        print_r(array_filter($time1->jogadores["tipo"],"G"));
 
 
     }
